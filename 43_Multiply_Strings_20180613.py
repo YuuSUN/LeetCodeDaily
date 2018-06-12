74697ed8ef865e13a6981717dde51a80aaa1ed8d
Fanhong <vivi3n@126.com>
2018-06-13 02:43:43 +0800

    43 solved

---
+++ 43_Multiply_Strings_20180613.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 13 01:58:42 2018
+
+@author: lifanhong
+
+The length of both num1 and num2 is < 110. —— what's the point of this?
+
+test case:
+    when output = "0"
+    
+"""
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        result = [0] * (len(num1) + len(num2) + 2)
+        output = ""
+        for i in range(len(num1)):
+            for j in range(len(num2)):
+                temp = int(num1[-1-i]) * int(num2[-1-j])
+                result[-1-i-j] += temp % 10
+                if temp >= 10:
+                    result[-2-i-j] += temp // 10 
+        for k in range(len(result)):
+            if result[-1-k] >= 10:
+                result[-2-k] += result[-1-k] // 10
+                result[-1-k] = result[-1-k] % 10
+        #print(result)
+        for digit in result:
+            if digit != 0 or output != "" :
+                output += str(digit)
+        if output == "":
+            output = "0"
+        return output
+        
+print(Solution().multiply("123","456"))
