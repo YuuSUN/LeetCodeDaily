e6114ab9ebd6b401a8b63266010bb1d75e23637f
Fanhong <vivi3n@126.com>
2018-08-31 17:41:04 +0800

    update 120

--- 120_20180830.py
+++ 120_20180830.py
@@ -12,6 +12,23 @@ class Solution:
         :type triangle: List[List[int]]
         :rtype: int
         """
+        self.triangle = triangle
+        self.branchmin = dict()
+        return self.miniSub(0,0)
+        
+        
+    def miniSub(self,i,j):
+        """
+        input: position of the tip
+        output:minimun of the triangle with tip i,j
+        """
+        if i == len(self.triangle) - 1:
+            return self.triangle[i][j]
+        else:
+            if (i,j) not in self.branchmin.keys():
+                self.branchmin[(i,j)] = min(self.miniSub(i+1,j), self.miniSub(i+1,j+1))
+            return self.triangle[i][j] + self.branchmin[(i,j)]
+        
         
 triangle = [
      [2],
@@ -19,4 +36,4 @@ triangle = [
    [6,5,7],
   [4,1,8,3]
 ]
-print(Solution().minimumTotal())
+print(Solution().minimumTotal(triangle))
